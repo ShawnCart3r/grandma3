b8d91409f023d908fdb681d5641ca1e6d38a21f5
Shawn Carter <53487411+ShawnCart3r@users.noreply.github.com>
2020-12-01 23:25:04 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,62 @@ function myFunction() {
       x.className = "navbar";
     }
   }
+
+  const urls = [
+    "https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2110&q=80",
+
+    "https://images.unsplash.com/photo-1573203425066-c8a62c0fa1fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+
+    "https://images.unsplash.com/photo-1550236520-7050f3582da0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+  ];
+
+  const countPic = urls.length;
+
+  let infinite = true;
+  let current = 0;
+  let nextUrl = 0;
+  let prevUrl = 0;
+
+  let bgStyle = document.querySelector(".section-hero").style;
+
+  bgStyle.backgroundImage = `url(${urls[0]}) `;
+  bgStyle.backgroundRepeat = "no-repeat";
+  bgStyle.backgroundPosition = "center";
+  bgStyle.backgroundSize = "cover";
+
+  function showPrev() {}
+
+  document.querySelector(".picnav-left").addEventListener("click", e => {
+    e.preventDefault();
+
+    if (current === 0) {
+      prevUrl = urls[countPic - 1];
+      current = countPic - 1;
+
+      if (!infinite) {
+        current = 0;
+        prevUrl = urls[current];
+      }
+    } else {
+      prevUrl = urls[--current];
+    }
+    bgStyle.backgroundImage = `url(${prevUrl})`;
+
+    console.log(current + " " + countPic);
+  });
+  document.querySelector(".picnav-right").addEventListener("click", e => {
+    e.preventDefault();
+
+    if (current === countPic - 1) {
+      current = 0;
+      nextUrl = urls[current];
+
+      if (!infinite) {
+        current = countPic - 1;
+        nextUrl = urls[current];
+      }
+    } else {
+      nextUrl = urls[++current];
+    }
+    bgStyle.backgroundImage = `url(${nextUrl})`;
+  });
